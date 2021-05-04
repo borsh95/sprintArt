d411e2e53414cb384377f8e614e31e96405464a3
ArtBro <artem.borsh95@gmail.com>
2021-05-04 14:02:45 +0300

    index-page: done:

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -30,27 +30,23 @@ if (document.querySelector('.search')) {
 	}
 }
 
-//Hamburger
-(function () {
+burgerMenu()
+function burgerMenu() {
 	const hamburgerBtn = document.querySelector('.hamburger');
 	const burgerBlock = document.querySelector('.header__burger');
 	const bodyEl = document.querySelector('body');
 
-
-
-	hamburgerBtn.addEventListener('click', function () {
-		this.classList.toggle('active');
-
-		workMenu();
-	});
+	hamburgerBtn.addEventListener('click', workMenu);
 
 	window.addEventListener('resize', function () {
-		if (document.documentElement.clientWidth > 1197 && burgerBlock.classList.contains('active')) {
+		if (document.documentElement.clientWidth > 1024 && burgerBlock.classList.contains('active')) {
 			workMenu();
 		}
 	});
 
 	function workMenu() {
+		hamburgerBtn.classList.toggle('active');
+
 		let coordsHamburgerBlock = burgerBlock.getBoundingClientRect();
 		let isActive = hamburgerBtn.classList.contains('active');
 
@@ -58,4 +54,43 @@ if (document.querySelector('.search')) {
 		burgerBlock.classList[isActive ? 'add' : 'remove']('active');
 		bodyEl.style.overflow = (isActive) ? 'hidden' : '';
 	}
-}());
+}
+
+//map 
+if (document.querySelector('#location')) {
+	ymaps.ready(init);
+	function init() {
+		// Создание карты.
+		var myMap = new ymaps.Map("location", {
+			controls: [],
+			center: [53.92967011903505, 27.384130772488792],
+			zoom: 14
+		});
+
+		myMap.behaviors.disable('drag');
+	}
+}
+
+const dropdownEl = document.querySelectorAll('[data-dropdown="wrapp"]');
+dropdownEl.forEach(item => dropdownList(item));
+
+//Выпадающий список
+function dropdownList(dropWrappEl) {
+	const dropdownList = dropWrappEl.querySelector('[data-dropdown="list"]');
+	const dropdownBtn = dropWrappEl.querySelector('[data-dropdown="btn"]');
+
+	dropdownBtn.addEventListener('click', toggleDropdown);
+
+	function toggleDropdown() {
+		this.classList.toggle('active');
+		dropWrappEl.classList.toggle('active');
+		isActive = dropdownBtn.classList.contains('active');
+
+		dropdownList.style.minHeight = isActive ? dropdownList.scrollHeight + 'px' : '';
+	}
+
+	return {
+		toggleDropdown
+	}
+}
+
